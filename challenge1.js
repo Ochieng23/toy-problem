a409e78d5ce89b260712d7133e2956fa165b56dc
+ <+>
2023-02-23 15:16:47 +0300

    done

--- challenge1.js
+++ challenge1.js
@@ -1,3 +1,3 @@
 const acceptsTwo = (a,b) => {
-    return {true: `${a} is greater than ${b}`, false: `${a} is equal to ${b}`}[a < b] || `${a} is smaller than ${b}`;
+    return {true: `${a} is greater than ${b}`, false: `${a} is equal to ${b}`}[a > b] || `${a} is smaller than ${b}`;
 }
